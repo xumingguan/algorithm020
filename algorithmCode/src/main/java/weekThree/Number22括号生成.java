c17d2d364312c831649cdf4cce39fc5f469b79b7
xumingguan <1063847690@qq.com>
2020-12-06 16:47:17 +0800

    提交  括号生成 的代码

--- algorithmCode/src/main/java/weekThree/Number22括号生成.java
+++ algorithmCode/src/main/java/weekThree/Number22括号生成.java
@@ -10,7 +10,10 @@ import java.util.List;
  */
 public class Number22括号生成 {
     public static void main(String[] args) {
-        recursion(0, 3, "");
+        // recursion(0, 3, "");
+        List<String> list = generateParenthesis(3);
+        System.out.println(list);
+
     }
 
     public static void recursion(int n, int max, String s) {
@@ -23,12 +26,6 @@ public class Number22括号生成 {
         recursion(n + 1, max, s + "右");
         System.out.println(s);
     }
-
-
-    private static List<String> generateParenthesisValue(int n, ArrayList<String> strings) {
-        return null;
-    }
-
 
     static List<String> res = new ArrayList<>();
 
@@ -50,13 +47,45 @@ public class Number22括号生成 {
         }
     }
 
-
-    public static int test(int n) {
-        if (n == 1) {
-            return 1;
+    static class Number22括号生成2 {
+        public static void main(String[] args) {
+            List<String> list = generateParenthesis(3);
+            System.out.println(list.toString());
         }
-        return test(n - 1);
-    }
 
+        public static List<String> generateParenthesis(int n) {
+            dfsTest(n, n, "");
+            return res;
+        }
+
+        private static void dfsTest(int left, int right, String s) {
+            if (left == 0 && right == 0) {
+                res.add(s);
+                return;
+            }
+            if (left > 0) {
+                dfsTest(left - 1, right, s + "(");
+            }
+            if (left < right) {
+                dfsTest(left, right - 1, s + ")");
+            }
+        }
+
+        private static void dfs(int left, int right, String curStr) {
+            // 左右括号都不剩余了，递归终止
+            if (left == 0 && right == 0) {
+                res.add(curStr);
+                return;
+            }
+            if (left > 0) { // 如果左括号还剩余的话，可以拼接左括号
+                dfs(left - 1, right, curStr + "(");
+            }
+            if (right > left) { // 如果右括号剩余多于左括号剩余的话，可以拼接右括号
+                dfs(left, right - 1, curStr + ")");
+            }
+        }
+
+
+    }
 
 }
